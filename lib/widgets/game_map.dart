5bdee24ff31f2295304bf0b1b0618246102b69cc
rtrydev <r.kulka44@gmail.com>
2023-01-28 17:58:45 +0100

    Added current location

--- lib/widgets/game_map.dart
+++ lib/widgets/game_map.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 
 import 'package:am_proj_app/services/i_waypoint_service.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../dependency_injection.dart';
 import '../models/waypoint_data.dart';
@@ -15,21 +17,36 @@ class GameMap extends StatefulWidget {
 }
 
 class GameMapState extends State<GameMap> {
-  List<WaypointData> waypoints = [];
   Set<Marker> markers = {};
+  bool locationEnabled = false;
+  LatLng location = const LatLng(49.783393, 19.057861);
 
   final Completer<GoogleMapController> _controller =
   Completer<GoogleMapController>();
 
   final IWaypointService waypointService = getIt<IWaypointService>();
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(49.783393, 19.057861),
-    zoom: 15.5,
-  );
-
   @override
   Widget build(BuildContext context) {
+    [
+      Permission.locationWhenInUse
+    ].request().then(
+        (result) {
+          if (result.isNotEmpty) {
+            Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+              .then((position) {
+                double lat = position.latitude;
+                double long = position.longitude;
+
+                setState(() {
+                  locationEnabled = true;
+                  location = LatLng(lat, long);
+                });
+            });
+          }
+        }
+    );
+
     waypointService.getWaypoints()
       .then((result) {
         setState(() {
@@ -46,11 +63,15 @@ class GameMapState extends State<GameMap> {
     return Scaffold(
       body: GoogleMap(
         mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
+        initialCameraPosition: CameraPosition(
+          target: location,
+          zoom: 14
+        ),
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
         markers: markers,
+        myLocationEnabled: locationEnabled
       )
     );
   }
